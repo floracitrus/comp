45f5290601e9c998f9407b4912a8f4504d7321cc
Flora Li <floracitrus@gmail.com>
2017-02-11 16:15:01 +1100

    Update removeDup.c

--- removeDup.c
+++ removeDup.c
@@ -9,7 +9,37 @@ struct node
     int data;
     struct node* next;
 };
+ /* Function to remove duplicates from a unsorted linked list */
+void removeDuplicates(struct node *start)
+{
+  struct node *ptr1, *ptr2, *dup;
+  ptr1 = start;
  
+  /* Pick elements one by one */
+  while(ptr1 != NULL && ptr1->next != NULL)
+  {
+     ptr2 = ptr1;
+ 
+     /* Compare the picked element with rest of the elements */
+     while(ptr2->next != NULL)
+     {
+       /* If duplicate then delete it */
+       if(ptr1->data == ptr2->next->data)
+       {
+          /* sequence of steps is important here */
+          dup = ptr2->next;
+          ptr2->next = ptr2->next->next;
+          free(dup);
+       }
+       else /* This is tricky */
+       {
+          ptr2 = ptr2->next;
+       }
+     }
+     ptr1 = ptr1->next;
+  }
+}
+
 /* The function removes duplicates from a sorted list */
 void removeDuplicates(struct node* head)
 {
