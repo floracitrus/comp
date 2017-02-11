9f08e15d5333acfe255e9f57f2ed1264cfb458a9
Flora Li <floracitrus@gmail.com>
2017-02-11 16:23:30 +1100

    iteration version of reverse link list

---
+++ reverse.c
@@ -0,0 +1,82 @@
+/*Input : Head of following linked list  
+       1->2->3->4->NULL
+Output : Linked list should be changed to,
+       4->3->2->1->NULL
+
+Input : Head of following linked list  
+       1->2->3->4->5->NULL
+Output : Linked list should be changed to,
+       5->4->3->2->1->NULL
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+ 
+/* Link list node */
+struct node
+{
+    int data;
+    struct node* next;
+};
+ 
+/* Function to reverse the linked list */
+static void reverse(struct node** head_ref)
+{
+    struct node* prev = NULL;
+    struct node* current = *head_ref;
+    struct node* next;
+    while(current != NULL){
+      next = current->next;//set up the "next" pointer
+      current->next = prev;//reverse the current next ptr
+      prev = current;//move a step for the prev
+      current = next;
+    }
+    *head_ref = prev;
+}
+ 
+/* Function to push a node */
+void push(struct node** head_ref, int new_data)
+{
+    /* allocate node */
+    struct node* new_node =
+            (struct node*) malloc(sizeof(struct node));
+            
+    /* put in the data  */
+    new_node->data  = new_data;
+                
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);    
+        
+    /* move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+ 
+/* Function to print linked list */
+void printList(struct node *head)
+{
+    struct node *temp = head;
+    while(temp != NULL)
+    {
+        printf("%d  ", temp->data);    
+        temp = temp->next;  
+    }
+}    
+ 
+/* Driver program to test above function*/
+int main()
+{
+    /* Start with the empty list */
+    struct node* head = NULL;
+   
+     push(&head, 20);
+     push(&head, 4);
+     push(&head, 15); 
+     push(&head, 85);      
+     
+     printf("Given linked list\n");
+     printList(head);    
+     reverse(&head);                      
+     printf("\nReversed Linked list \n");
+     printList(head);    
+     getchar();
+}
